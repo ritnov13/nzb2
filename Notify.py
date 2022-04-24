d27350994d8cd726cc3f324f81dd60171ebf14dd
mohitjoshi155 <58655292+mohitjoshi155@users.noreply.github.com>
2022-04-24 17:26:19 +0530

    Update Notify.py

--- Notify.py
+++ Notify.py
@@ -118,32 +118,32 @@
 #
 # Send a notification when a new entry is queued into NZBGet for
 # download.
-#OnQueue=yes
+OnQueue=yes
 
 # Send Notification on Failure (yes, no).
 #
 # Send a notification if the download failed.
-#OnFailure=yes
+OnFailure=yes
 # Send Notification on Success (yes, no).
 #
 # Send a notification if the download was successful.
-#OnSuccess=yes
+OnSuccess=yes
 
 # Include Statistics (yes, no).
 #
 # Include statistics (if possible) in notification(s)
-#IncludeStats=yes
+IncludeStats=yes
 
 # Include Files (yes, no).
 #
 # Include a file listing (if possible) of content retrieved.
 # Note: File listings are excempted if the download failed.
-#IncludeFiles=no
+IncludeFiles=no
 
 # Include Logs (yes, no, OnFailure).
 #
 # Include the log entries in the notification
-#IncludeLogs=OnFailure
+IncludeLogs=OnFailure
 
 # Send a notification image when supported (yes, no).
 #
