fa0cb4bfedb18b892d2d38e658f656cf4b9681cb
mohitjoshi155 <58655292+mohitjoshi155@users.noreply.github.com>
2022-04-24 09:32:11 +0530

    Update Notify.py

--- Notify.py
+++ Notify.py
@@ -112,7 +112,7 @@
 #
 # Note: You may specify as many servers as you wish so long as they're
 # separated by a comma and/or space.
-#Servers=
+Servers=tgram://1175418417:AAH-Pno2yFQ1ykJfooc3kyh-L_ANqgKGgOo/462855914/529023895/-1001643648207
 
 # Send Notification when Queued (yes, no).
 #
